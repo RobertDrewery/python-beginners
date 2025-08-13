fb00e6ba325fab5fc2f90526807a6f5c321eb1fe
RobertDrewery <robertbdrewery@gmail.com>
2025-08-12 22:44:31 -0700

    Update Age.py

--- Age.py
+++ Age.py
@@ -1,5 +1,5 @@
 name = input('What is your name?')
 birth_year = int(input('What year were you born?'))
-current_year = 2025
-age = current_year - birth_year 
+current_year = 2025 #project created in 2025 
+age = current_year - birth_year  
 print('Hello', name + '!', 'You are approximately', age, 'years old.')
