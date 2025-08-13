bdc2708785f5fdbafbd18a879d016599cc83dea7
RobertDrewery <robertbdrewery@gmail.com>
2025-08-12 22:42:16 -0700

    Add files via upload
    Approximate Age Calculator

---
+++ Age.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+birth_year = int(input('What year were you born?'))
+current_year = 2025
+age = current_year - birth_year 
+print('Hello', name + '!', 'You are approximately', age, 'years old.')
